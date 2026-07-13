b0db5aa47c045c9a1cae577e84ab2d97de13d942
jiantai <Jesencloud>
2026-07-13 17:35:15 +0800

    fix: 仅在保存日期与当前日期一致时更新日程列表，避免异步覆盖，并补充回归测试

--- pages/index/formHandlers.js
+++ pages/index/formHandlers.js
@@ -7,6 +7,7 @@ const {
   createCustomCategory,
   resolveCategory
 } = require('../../utils/categories.js');
+const { cleanMemosUIFields } = require('../../utils/backup.js');
 
 const DEFAULT_FORM = {
   id: '',
@@ -159,12 +160,14 @@ module.exports = {
       this.showToast(text.saved, 'success');
       this.memoDates = updatedMemoDates;
 
-      this.clearSwipeCloseTimer();
-      closeStarted = this._closeModalWithData({
+      const closeData = {
         memoDateMeta: this.updateMemoDateMeta(this.data.memoDateMeta, selectedDate, dayMemos),
-        selectedMemos: cleanMemosUIFields(dayMemos),
         swipedMemoId: ''
-      }, () => {
+      };
+      if (this.data.selectedDate === selectedDate) {
+        closeData.selectedMemos = cleanMemosUIFields(dayMemos);
+      }
+      closeStarted = this._closeModalWithData(closeData, () => {
         this.finishBusyState('savingMemo');
       });
     } finally {

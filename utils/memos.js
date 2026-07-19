f87f4cc81d1bb4f18c0044ccdc07a9132c4f79fb
jiantai <Jesencloud>
2026-07-19 15:44:10 +0800

    perf: 优化 cleanMemosUIFields 以短路检测避免干净数组的重复克隆与分配

--- utils/memos.js
+++ utils/memos.js
@@ -1,6 +1,11 @@
 function cleanMemosUIFields(memos) {
   if (!Array.isArray(memos)) return [];
+  
+  const hasDirty = memos.some(item => 'isSwiped' in item);
+  if (!hasDirty) return memos;
+
   return memos.map(item => {
+    if (!('isSwiped' in item)) return item;
     const cleanItem = Object.assign({}, item);
     delete cleanItem.isSwiped;
     return cleanItem;
@@ -8,8 +13,21 @@ function cleanMemosUIFields(memos) {
 }
 
 function cleanMemoDatesUIFields(memoDates) {
+  if (!memoDates) return {};
+  
+  let hasDirty = false;
+  for (const date in memoDates) {
+    const list = memoDates[date];
+    if (Array.isArray(list) && list.some(item => 'isSwiped' in item)) {
+      hasDirty = true;
+      break;
+    }
+  }
+
+  if (!hasDirty) return memoDates;
+
   const cleanMemoDates = {};
-  Object.keys(memoDates || {}).forEach(date => {
+  Object.keys(memoDates).forEach(date => {
     const list = memoDates[date];
     cleanMemoDates[date] = Array.isArray(list) ? cleanMemosUIFields(list) : list;
   });
